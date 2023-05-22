2fcd718c1aa18b90c293009d1d486c1940895486
OlafRyznar <olaf.ryznar.20@gmail.com>
2023-05-22 09:22:24 +0200

    super

--- main.py
+++ main.py
@@ -18,11 +18,22 @@ async def create_student(student: StudentCreateSchema):
     """
     Endpoint that creates a new student.
     """
-    
+    if any(char.isdigit() for char in student.first_name) or any(char.isdigit() for char in student.last_name):
+        raise HTTPException(
+            status_code=400, detail="Student information cannot contain digits")
+    if not student.first_name or not student.last_name:
+        raise HTTPException(
+            status_code=400, detail="Student information cannot be empty")
+    existing_ids = [student_id for student_id, s in students.items(
+    ) if s.first_name == student.first_name and s.last_name == student.last_name]
+    if len(existing_ids) > 1 or (len(existing_ids) == 1 and existing_ids[0] != id):
+        raise HTTPException(
+            status_code=400, detail="Duplicate student information")
+
     student_id = len(students) + 1
     students[student_id] = student
     return {"id": student_id, **student.dict()}
-    
+
 
 @app.get("/students")
 async def list_students():
@@ -47,8 +58,6 @@ async def get_student(id: int):
     return {"id": id, **students[id].dict()}
 
 
-
-
 @app.patch("/student/{id}")
 async def update_student(id: int, student: StudentUpdateSchema):
     """
@@ -56,9 +65,9 @@ async def update_student(id: int, student: StudentUpdateSchema):
     """
     if id not in students:
         raise HTTPException(status_code=404, detail="Student not found")
-    if isinstance(student.first_name, "1") or isinstance(student.last_name, "1"):
+    if any(char.isdigit() for char in student.first_name) or any(char.isdigit() for char in student.last_name):
         raise HTTPException(
-            status_code=400, detail="Student information must be strings")
+            status_code=400, detail="Student information cannot contain digits")
     if not student.first_name or not student.last_name:
         raise HTTPException(
             status_code=400, detail="Student information cannot be empty")
