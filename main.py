7c9afcbbff6e7815273e3c292877d7f58572a7a6
OlafRyznar <olaf.ryznar.20@gmail.com>
2023-05-22 08:07:06 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class StudentCreateSchema(BaseModel):
+    first_name: str
+    last_name: str
+    
+@app.post("/students/")
+async def create_item(item: StudentCreateSchema):
+    return item
